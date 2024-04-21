78bfbe2150eb99f6d75cbb6c97c6980505258423
Alone2345 <167343720+Alone2345@users.noreply.github.com>
2024-04-21 15:19:49 +0300

    Normal program on C++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+float P_rectangle(float a, float b) {
+    return 2 * (a + b);
+}
+
+float S_rectangle(float a, float b) {
+    return a * b;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    float a, b;
+    cin >> a >> b;
+    int sum = P_rectangle(a, b);
+    int multiplication = S_rectangle(a, b);
+    cout << "Периметр прямоугольника: " << sum << endl;
+    cout << "Площадь прямоугольника: " << multiplication << endl;
+    return 0;
+}
